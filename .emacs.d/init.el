1bd822f787adf6d866be90555bb8a27277343dad
MisanthropicBit <alexander.asp.bock@gmail.com>
2017-12-25 01:42:08 +0100

    Add emacs configuration file

---
+++ .emacs.d/init.el
@@ -0,0 +1,45 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+;(setq package-enable-at-startup nil)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (helm evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq evil-want-C-u-scroll t)
+(require 'evil)
+(evil-mode t)
+(define-key evil-normal-state-map [jk] 'keyboard-quit)
+(define-key evil-visual-state-map [jk] 'keyboard-quit)
+
+; Don't display the startup message
+(setq inhibit-startup-message t)
+
+; No toolbar in GUI
+(tool-bar-mode -1)
+
+; No scrollbar
+(when (display-graphic-p) (set-scroll-bar-mode nil))
+
+; Set cursor colors depending on vi mode
+(when (display-graphic-p)
+  (setq evil-emacs-state-cursor '("red" box))
+  (setq evil-normal-state-cursor '("green" box))
+  (setq evil-visual-state-cursor '("orange" box))
+  (setq evil-insert-state-cursor '("red" bar))
+  (setq evil-replace-state-cursor '("red" bar))
+  (setq evil-operator-state-cursor '("red" hollow))
+)
