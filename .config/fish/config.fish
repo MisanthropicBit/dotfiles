b307eb1bbf05dcca52df8137a22e556d0082cd99
MisanthropicBit <alexander.asp.bock@gmail.com>
2019-08-05 23:52:43 +0200

    Add edtior vars, pyenv, aliases and macports

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,21 @@
-set --universal EDITOR vim
-set --universal VISUAL vim
+set --universal EDITOR                 nvim
+set --universal GIT_EDITOR             nvim
+set --universal VISUAL                 nvim
 set --universal FISH_POWERPROMPT_THEME "random"
 
 # Colors
 set --universal fish_color_command 00c5d7 darkcyan
 set -gx LSCOLORS ExfxxxxxBx
 
-source ./aliases.fish
+if type -q ~/aliases.fish
+    source ~/aliases.fish
+end
+
+# Add macports bin/ directory
+set PATH /opt/local/bin $PATH
+
+# Add pyenv to path
+set --universal PYENV_ROOT "$HOME/.pyenv"
+set PATH "$PYENV_ROOT/bin" $PATH
+
+status --is-interactive; and source (pyenv init -|psub)
