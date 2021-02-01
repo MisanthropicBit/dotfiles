193af5cc3d0f17f106ea61439277ec849acf4db0
MisanthropicBit <albo.developer@gmail.com>
2021-02-01 20:13:54 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+files=".agignore .aliases .bashrc .config .gitconfig .inputrc .latexmkrc .profile .vimrc .vim"
+
+# Check for pre-exisiting files
+for file in $files; do
+    if -e "~/.$file"; then
+        printf "File '.$file' already exists in your home directory, aborting...\n"
+        exit 1
+    fi
+done
+
+# Symlink files into user home directory
+for file in $files; do
+    printf "Symlinking '.$file'...\n"
+    ln -s ".$file" "~/.$file"
+
+    if [$? -ne 0]; then
+        printf "Failed to symlink '.$file'!"
+    fi
+done
+
 __symlink_install() {
     if [ -e "$3/$1" ]; then
         echo "Error: Cannot symlink $1 because it already exists in $3"
