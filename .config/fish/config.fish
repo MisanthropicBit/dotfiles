5bb0a882f48cf2ca3db0251fa192105350353d7c
aab <aab@connectedcars.dk>
2022-08-03 20:20:09 +0200

    Add MySQL PS1, bat theme, fzf envvars, work config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,3 +55,12 @@ for nigiri_file in $nigiri_files
         source $nigiri_file
     end
 end
+
+set -x MYSQL_PS1 "mysql v\v [\U][\d]> "
+set -x BAT_THEME zenburn
+set -x FZF_DEFAULT_COMMAND "fd -tf --color=never"
+set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --search-path \$dir"
+set -x FZF_CTRL_T_OPTS '--multi --bind="ctrl-s:select" --bind="ctrl-u:deselect" --bind="ctrl-f:reload(find . -type f \$dir)" --bind="ctrl-d:reload(find . -type d \$dir)" --preview="bat -n --color=always --line-range :20 {}"'
+set -x FZF_ALT_C_COMMAND 'fd -td --color=never'
+
+source ~/.work-config.fish
