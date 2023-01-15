63238ef7089df439fe02c0e40b0002d3152e75ee
aab <aab@connectedcars.dk>
2023-01-15 12:53:04 +0100

    Parse ansi codes, fix placeholder for dh

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -92,7 +92,7 @@ end
 function dh -d "Fuzzy search directory history"
     set -l result (
         dirh |
-        fzf --with-nth=2 --tiebreak=end --preview='tree -C -L 1 -hp --dirsfirst {2}' |
+        fzf --ansi --with-nth=2 --tiebreak=end --preview='tree -C -L 1 -hp --dirsfirst {1}' |
         string split -f2 ') '
     ); and cd "$result"
 end
