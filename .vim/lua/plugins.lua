16470cc334543335949a69cb1dc176468b613124
aab <aab@connectedcars.dk>
2023-01-21 11:06:21 +0100

    Customise lsp_signature setup

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -296,7 +296,13 @@ require('boole').setup({
 -- }}}
 
 -- lsp_signature.nvim {{{
-require('lsp_signature').setup()
+require('lsp_signature').setup{
+    close_timeout = 2000,
+    always_trigger = false,
+    hint_prefix = ' ﴕ ',
+    select_signature_key = '<c-s>',
+    timer_interval = 100,
+}
 -- }}}
 
 -- indent-blankline.nvim {{{
