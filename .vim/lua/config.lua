b3b1647979d59173fe12649bcd47285b1b781232
aab <aab@connectedcars.dk>
2023-01-22 17:00:58 +0100

    Load private plugins in main config

--- .vim/lua/config.lua
+++ .vim/lua/config.lua
@@ -4,6 +4,7 @@ require('plugins')
 require('lsp')
 
 local map_options = require('mappings').map_options
+pcall(require, 'private_plugins')
 
 -- Text yank highlight {{{
 local augroup = vim.api.nvim_create_augroup
