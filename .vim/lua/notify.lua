052191ec0c4c79689ed7c7538b3e661ac61f6e96
aab <aab@connectedcars.dk>
2023-02-10 22:24:53 +0100

    Add mac notify module

---
+++ .vim/lua/notify.lua
@@ -0,0 +1,17 @@
+local notify = {}
+
+local old_vim_notify = vim.notify
+local osascript_cmd = "osascript -e 'display notification \"%s\" with title \"%s\"'"
+
+vim.notify = function(msg, level, opts)
+    if type(opts) == 'table' and opts.mac == true then
+        local title = (opts.title or 'neovim')
+
+        -- print(string.format(osascript_cmd, msg, title))
+        vim.fn.system(string.format(osascript_cmd, msg, title))
+    else
+        old_vim_notify(msg, level, opts)
+    end
+end
+
+return notify
