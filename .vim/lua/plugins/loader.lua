d5ec99068209e26d85a8e8eeabc6eb0336587008
MisanthropicBit <albo.developer@gmail.com>
2023-02-12 12:52:47 +0100

    Improve loader.load function

--- .vim/lua/plugins/loader.lua
+++ .vim/lua/plugins/loader.lua
@@ -1,12 +1,17 @@
 local loader = {}
 
 function loader.load(plugin_name)
-    local plugin_exists, plugin = pcall(require, plugin_name)
+    local plugin_exists, _ = pcall(require, plugin_name)
 
     if plugin_exists then
         -- If plugin exists, load the setup file
         local plugin_config = 'plugins.' .. plugin_name
-        pcall(require, plugin_config)
+        local status, maybe_error = pcall(require, plugin_config)
+
+        if not status then
+            local msg = string.format("Failed to run plugin setup for '%s': %s", plugin_name, maybe_error)
+            vim.api.nvim_echo({{ msg, 'ErrorMsg' }}, true, {})
+        end
     else
         local msg = string.format("Failed to load plugin '%s'", plugin_name)
         vim.api.nvim_echo({{ msg, 'ErrorMsg' }}, true, {})
