34514fa7064b0d536e45b55a6b33058e77d2170b
aab <aab@connectedcars.dk>
2023-03-31 21:53:06 +0200

    Rename ansi module, add extra utility functions

--- .vim/lua/ansi.lua
+++ .vim/lua/ansi.lua
@@ -1,9 +1,9 @@
-local M = {}
+local ansi = {}
 
 local bit = require('bit')
 
 -- Extract the RGB components from a color number
-function M.extract_rgb(color)
+function ansi.extract_rgb(color)
     local r = bit.band(bit.rshift(color, 16), 0xff)
     local g = bit.band(bit.rshift(color, 8), 0xff)
     local b = bit.band(bit.rshift(color, 0), 0xff)
@@ -12,26 +12,47 @@ function M.extract_rgb(color)
 end
 
 -- Convert a color number to an ANSI escape code
-function M.rgb_to_ansi(encoded_color, is_bg)
+function ansi.rgb_to_ansi(encoded_color, is_bg)
     if encoded_color == nil then
         return ''
     end
 
-    local r, g, b = M.extract_rgb(encoded_color)
-    local ansi_code = is_bg and '48' or '38' 
+    local r, g, b = ansi.extract_rgb(encoded_color)
+    local ansi_code = is_bg and '48' or '38'
 
     return string.format('\x1b[%s;2;%s;%s;%sm', ansi_code, r, g, b)
 end
 
 -- Convert a vim highlight group to an RGB ANSI escape code
 ---@type string hl_name
-function M.highlight_to_rgb_ansi(hl_name)
+function ansi.highlight_to_rgb_ansi(hl_name)
     local colors = vim.api.nvim_get_hl_by_name(hl_name, true)
     -- local colors = { foreground = 16394340 }
-    local ansi = M.rgb_to_ansi(colors.foreground, false)
-    ansi = ansi .. M.rgb_to_ansi(colors.background, true)
+    local _ansi = ansi.rgb_to_ansi(colors.foreground, false)
+    _ansi = _ansi .. ansi.rgb_to_ansi(colors.background, true)
 
-    return ansi
+    return _ansi
 end
 
-return M
+--- Return a pattern for matching ansi escape sequences
+---@return string
+function ansi.pattern()
+    return '\x1b%[[0-9;K]+m'
+end
+
+--- Return the ansi escape sequence for resetting escape sequences
+---@return string
+function ansi.reset_sequence()
+    return '\x1b[0m'
+end
+
+--- Strip ansi escape sequences from a string
+---@param value string
+---@return string
+function ansi.strip_ansi_codes(value)
+    local stripped, _ = value:gsub(ansi.pattern(), '')
+
+    return stripped
+end
+
+return ansi
