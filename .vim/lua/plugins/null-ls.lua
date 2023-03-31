c4c9d93bce12d1194c5b61df3f20defd3ee40220
aab <aab@connectedcars.dk>
2023-03-31 22:10:57 +0200

    Set up :Lsp*-like commands for null-ls

--- .vim/lua/plugins/null-ls.lua
+++ .vim/lua/plugins/null-ls.lua
@@ -12,3 +12,36 @@ null_ls.setup{
         null_ls.builtins.diagnostics.eslint_d,
     }
 }
+
+---@param command_func fun(client: vim.lsp.client)
+local function null_ls_command(command_func)
+    return function()
+        local null_ls_client
+
+        for _, client in ipairs(vim.lsp.get_active_clients()) do
+            if client.name == 'null-ls' then
+                null_ls_client = client
+            end
+        end
+
+        if not null_ls_client then
+            return
+        end
+
+        command_func(null_ls_client)
+    end
+end
+
+local function null_ls_stop(client)
+    if not client.is_stopped() then
+        client.stop()
+    end
+end
+
+local function null_ls_restart(client)
+    null_ls_stop(client)
+    vim.lsp.start_client(client.config)
+end
+
+vim.api.nvim_create_user_command('NullLsStop', null_ls_command(null_ls_stop), {})
+vim.api.nvim_create_user_command('NullLsRestart', null_ls_command(null_ls_restart), {})
