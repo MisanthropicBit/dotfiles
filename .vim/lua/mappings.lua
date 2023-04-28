bfbab5882a0cc17999a0ce97b02c4eb6047e06eb
aab <aab@connectedcars.dk>
2023-04-28 20:03:31 +0200

    Rename to map, add annotations

--- .vim/lua/mappings.lua
+++ .vim/lua/mappings.lua
@@ -1,29 +1,43 @@
-local M = {}
+local map = {}
 
-M.default_options = { noremap = true, silent = true }
+---@alias Mode 'n' | 'v' | 'x' | 's' | 'o' | 'i' | 'l' | 'c' | 't'
 
-function M.merge(opts1, opts2)
-    return vim.tbl_extend('force', opts1, opts2)
+---@type table
+map.default_options = { noremap = true, silent = true }
+
+---@param opts1 table
+---@param opts2 table?
+function map.merge(opts1, opts2)
+    return vim.tbl_extend('force', opts1, opts2 or {})
 end
 
-function M.with_default_options(opts)
-    return M.merge(M.default_options, opts)
+---@param opts table?
+function map.with_default_options(opts)
+    return map.merge(map.default_options, opts)
 end
 
-function M.set(mode, lhs, rhs, opts)
-    vim.keymap.set(mode, lhs, rhs, M.with_default_options(opts))
+---@param mode Mode
+---@param lhs string
+---@param rhs string | fun()
+---@param opts table?
+function map.set(mode, lhs, rhs, opts)
+    vim.keymap.set(mode, lhs, rhs, map.with_default_options(opts))
 end
 
-function M.leader(mode, lhs, rhs, opts)
+---@param mode Mode
+---@param lhs string
+---@param rhs string | fun()
+---@param opts table?
+function map.leader(mode, lhs, rhs, opts)
     local leader_lhs = '<localleader>' .. lhs
 
-    vim.keymap.set(mode, leader_lhs, rhs, M.with_default_options(opts))
+    vim.keymap.set(mode, leader_lhs, rhs, map.with_default_options(opts))
 end
 
 for _, mode in ipairs({ 'n', 'i', 'c', 'v', 'x', 's', 'o', 't', 'l' }) do
-    M[mode] = function(lhs, rhs, opts)
-        M.set(mode, lhs, rhs, opts)
+    map[mode] = function(lhs, rhs, opts)
+        map.set(mode, lhs, rhs, opts)
     end
 end
 
-return M
+return map
