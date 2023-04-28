5bdc895c3b20ebd2eedb3777df6235103dbc16d5
aab <aab@connectedcars.dk>
2023-04-28 20:21:15 +0200

    Autoload plugins from a path

--- .vim/lua/plugins/init.lua
+++ .vim/lua/plugins/init.lua
@@ -1,25 +1,3 @@
-local plugins = {
-    'boole',
-    'bufferline',
-    'dashboard',
-    'fzf-lua',
-    'goto-preview',
-    'indent_blankline',
-    'lsp_signature',
-    'lspsaga',
-    'neotest',
-    'null-ls',
-    'cmp',
-    'dapui',
-    'dap',
-    'nvim-treesitter-playground',
-    'nvim-treesitter',
-    'trouble',
-    'ts-node-action',
-}
-
 local loader = require('plugins.loader')
 
-table.foreach(plugins, function(idx, value)
-    loader.load(value)
-end)
+loader.load_plugins(vim.fn.stdpath('config') .. '/' .. 'lua/plugins')
--- .vim/lua/plugins/loader.lua
+++ .vim/lua/plugins/loader.lua
@@ -1,20 +1,57 @@
 local loader = {}
 
+local exclude_dirs = { 'init', 'loader', 'nvim-bqf', 'other' }
+
+--- Load a plugin
+---@param plugin_name string
+---@return boolean, any
+local function load_plugin(plugin_name)
+    local plugin_config = 'plugins.' .. plugin_name
+
+    return pcall(require, plugin_config)
+end
+
+--- Load a plugin
+---@param plugin_name string
 function loader.load(plugin_name)
     local plugin_exists, _ = pcall(require, plugin_name)
 
     if plugin_exists then
-        -- If plugin exists, load the setup file
-        local plugin_config = 'plugins.' .. plugin_name
-        local status, maybe_error = pcall(require, plugin_config)
+        local status, maybe_error = load_plugin(plugin_name)
 
         if not status then
             local msg = string.format("Failed to run plugin setup for '%s': %s", plugin_name, maybe_error)
             vim.api.nvim_echo({{ msg, 'ErrorMsg' }}, true, {})
+        else
+            -- Silently attempt to load a work-related plugin setup
+            load_plugin('work.' .. plugin_name)
         end
     else
         local msg = string.format("Failed to load plugin '%s'", plugin_name)
         vim.api.nvim_echo({{ msg, 'ErrorMsg' }}, true, {})
+    end
+end
+
+--- Load all plugins from a path
+---@param path string
+function loader.load_plugins(path)
+    if vim.fn.isdirectory(path) == 0 then
+        vim.api.nvim_echo({{
+            string.format("Plugin path is not a directory: '%s'", path),
+            'ErrorMsg'
+        }}, true, {})
+
+        return
+    end
+
+    for name, type in vim.fs.dir(path) do
+        if type == 'file' then
+            local plugin_name = vim.fn.fnamemodify(name, ':p:t:r')
+
+            if not vim.tbl_contains(exclude_dirs, plugin_name) then
+                loader.load(plugin_name)
+            end
+        end
     end
 end
 
