817d8335db07d7fda0c0788f713f423adde27e66
aab <aab@connectedcars.dk>
2023-06-03 16:48:57 +0200

    Silence duplicate field warning

--- .vim/lua/notify.lua
+++ .vim/lua/notify.lua
@@ -3,6 +3,7 @@ local notify = {}
 local old_vim_notify = vim.notify
 local osascript_cmd = "osascript -e 'display notification \"%s\" with title \"%s\"'"
 
+---@diagnostic disable-next-line: duplicate-set-field
 vim.notify = function(msg, level, opts)
     if type(opts) == 'table' and opts.mac == true then
         local title = (opts.title or 'neovim')
