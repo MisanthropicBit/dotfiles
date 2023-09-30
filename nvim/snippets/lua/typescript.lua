209d2eae5a50d0b3b8f36a1a17ced42a9750e53b
MisanthropicBit <albo.developer@gmail.com>
2023-09-30 17:00:52 +0200

    Better typescript import/export snippets

--- nvim/snippets/lua/typescript.lua
+++ nvim/snippets/lua/typescript.lua
@@ -1,17 +1,31 @@
 local ls = require('luasnip')
+local c = ls.choice_node
 local s = ls.snippet
+local sn = ls.snippet_node
 local i = ls.insert_node
+local t = ls.text_node
 local fmt = require('luasnip.extras.fmt').fmt
 
 local fmta = ls.extend_decorator.apply(fmt, { delimiters = '<>' })
 
+local function get_import_export_fmt_args()
+    return {
+        c(
+            2, {
+                sn(nil, { t("{ "), i(1), t(" }") }),
+                sn(nil, { t("type { "), i(1), t(" }") }),
+                sn(nil, { t("* as "), i(1) }),
+            }
+        ),
+        i(1)
+    }
+end
+
 return {
-    s("im", fmt("import {} from '{}'", { i(2), i(1) })),
-    s("imt", fmt("import type {} from '{}'", { i(2), i(1) })),
-    s("ea", fmt("export * as {} from '{}'", { i(2), i(1) })),
-    s("ef", fmta("export { <> } from '<>'", { i(2), i(1) })),
     s("eaf", fmta([[export async function <>(): <> {
   <>
 }]], { i(1), i(2), i(3) })),
     s("et", fmt("export type {} = {}", { i(1), i(2) })),
+    s("im", fmta("import <> from '<>'", get_import_export_fmt_args())),
+    s("ex", fmta("export <> from '<>'", get_import_export_fmt_args()))
 }
