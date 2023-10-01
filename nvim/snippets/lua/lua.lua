b85b2936c83e22107d1b9643e431aa7ce33ca7e1
MisanthropicBit <albo.developer@gmail.com>
2023-10-01 10:34:51 +0200

    Proper indents

--- nvim/snippets/lua/lua.lua
+++ nvim/snippets/lua/lua.lua
@@ -1,10 +1,10 @@
 return {
     s("req", fmt('local {} = require("{}")', { i(1), rep(1) })),
     s("lf", fmt([[local function {}({})
-  {}
+    {}
 end]], { i(1), i(2), i(3) })),
     s("mf", fmt([[function {}.{}({})
-  {}
+    {}
 end]], { i(1), i(2), i(3), i(4) })),
     s("mod", fmt([[local {} = {{}}
 
