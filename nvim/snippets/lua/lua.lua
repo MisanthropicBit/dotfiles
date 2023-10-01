52126ed9bc1eee8d0d6dde8424e9d034ff1b5c9b
MisanthropicBit <albo.developer@gmail.com>
2023-10-01 21:01:33 +0200

    Add sections, add snippets for busted

--- nvim/snippets/lua/lua.lua
+++ nvim/snippets/lua/lua.lua
@@ -1,5 +1,6 @@
 return {
-    s("req", fmt('local {} = require("{}")', { i(1), rep(1) })),
+    -- General
+    s("req", fmt('local {} = require("{}")', { i(1), i(2) })),
     s("lf", fmt([[local function {}({})
     {}
 end]], { i(1), i(2), i(3) })),
@@ -11,5 +12,16 @@ end]], { i(1), i(2), i(3), i(4) })),
 {}
 
 return {}]], { i(1, "M"), i(2), rep(1) })),
-    s("vp", fmt("vim.print({})", i(1)))
+    s("vp", fmt("vim.print({})", i(1))),
+
+    -- Busted
+    s("des", fmt([[describe("{}", function()
+    {}
+end)]], { i(1), i(2) })),
+    s("it", fmt([[it("{}", function()
+    {}
+end)]], { i(1), i(2) })),
+    s("ita", fmt([[async.it("{}", function()
+    {}
+end)]], { i(1), i(2) })),
 }
