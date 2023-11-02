213b315156902c4a602b529902f25215f3c6a58e
aab <aab@connectedcars.dk>
2023-11-02 16:59:47 +0100

    Use double-quoted strings

--- nvim/lua/config/fold.lua
+++ nvim/lua/config/fold.lua
@@ -6,7 +6,7 @@ local has_ts_utils, ts_utils = pcall(require, "nvim-treesitter.ts_utils")
 function _G.custom_fold_text()
     local line = vim.fn.trim(vim.fn.getline(vim.v.foldstart))
     local lines_count = vim.v.foldend - vim.v.foldstart + 1
-    local format = '%s%s%s %d lines: %s '
+    local format = "%s%s%s %d lines: %s "
     local level = icons.folds.char:rep(vim.fn.foldlevel(vim.v.foldstart))
 
     if has_ts_utils then
@@ -15,15 +15,15 @@ function _G.custom_fold_text()
         if enclosing_function_node ~= nil then
             local lnum = enclosing_function_node:start()
 
-            line = 'Body of ' .. vim.fn.getline(lnum + 1)
+            line = "Body of " .. vim.fn.getline(lnum + 1)
         end
     end
 
-    local indent_level, _ = line:find('[^%s]')
+    local indent_level, _ = line:find("[^%s]")
     local indent = (' '):rep(indent_level - 1)
 
     return format:format(indent, icons.folds.marker .. '  ', level, lines_count, line)
 end
 
 vim.opt.fillchars = { fold = icons.folds.char }
-vim.o.foldtext = 'v:lua.custom_fold_text()'
+vim.o.foldtext = "v:lua.custom_fold_text()"
