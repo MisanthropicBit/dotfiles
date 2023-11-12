f9186fc83c00b885b268fc22ac816c5942888fb6
MisanthropicBit <albo.developer@gmail.com>
2023-11-12 17:20:19 +0100

    Use vim.ui.input and fzf-lua for dap input

--- nvim/lua/config/plugins/dap.lua
+++ nvim/lua/config/plugins/dap.lua
@@ -10,17 +10,28 @@ local fzf_lua_loaded, fzf_lua_core = pcall(require, "fzf-lua.core")
 local select_executable_prompt = "Select executable " .. icons.misc.prompt
 
 local function select_executable()
-    return vim.fn.input(select_executable_prompt, vim.fn.getcwd() .. "/", "file")
+    return vim.fn.input({
+        prompt = select_executable_prompt,
+        default = vim.fn.getcwd() .. "/",
+        completion = "file",
+    })
 end
 
-if not fzf_lua_loaded then
+if fzf_lua_loaded then
     select_executable = function()
-        fzf_lua_core.fzf_exec("fd --type executable", {
-            prompt = select_executable_prompt,
-            cwd = vim.fn.getcwd(),
-            winopts = { width = 0.6, height = 0.5 },
-            fzf_opts = { ["--pointer"] = debugging.breakpoint },
-        })
+        return coroutine.create(function(dap_run_co)
+            fzf_lua_core.fzf_exec("fd --type executable", {
+                prompt = select_executable_prompt,
+                cwd = vim.fn.getcwd(),
+                winopts = { width = 0.6, height = 0.5 },
+                fzf_opts = { ["--pointer"] = debugging.breakpoint },
+                actions = {
+                    ["enter"] = function(selected)
+                        coroutine.resume(dap_run_co, selected[1])
+                    end,
+                },
+            })
+        end)
     end
 end
 
@@ -82,14 +93,21 @@ dap.configurations.cpp = {
         cwd = "${workspaceFolder}",
         stopOnEntry = false,
         args = function()
-            local args = {}
-            local args_string = vim.fn.input("Program arguments " .. icons.misc.prompt)
+            return coroutine.create(function(dap_run_co)
+                vim.ui.input({
+                    prompt = "Program arguments " .. icons.misc.prompt .. " ",
+                    completion = "file",
+                },
+                function(input)
+                    local args = {}
 
-            for word in args_string:gmatch("%S+") do
-                table.insert(args, word)
-            end
+                    for word in input:gmatch("%S+") do
+                        table.insert(args, word)
+                    end
 
-            return args
+                    coroutine.resume(dap_run_co, args)
+                end)
+            end)
         end,
     },
 }
