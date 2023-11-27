f49c973f52a6dd0bfc83d6b9c851d563d577bed2
aab <aab@connectedcars.dk>
2023-11-27 20:51:46 +0100

    Migrate to indent_blankline v3

--- nvim/lua/config/plugins/indent_blankline.lua
+++ nvim/lua/config/plugins/indent_blankline.lua
@@ -1,6 +1,17 @@
-require("indent_blankline").setup({
-    show_current_context = true,
-    show_current_context_start = true,
-    char = "",
-    context_char = "│",
+local default_exclude_filetypes = vim.list_slice(require("ibl.config").default_config.exclude.filetypes)
+
+vim.list_extend(default_exclude_filetypes, { "dashboard", "fugitive" })
+
+require("ibl").setup({
+    enabled = true,
+    indent = {
+        char = "│",
+    },
+    scope = {
+        enabled = true,
+        show_start = true,
+    },
+    exclude = {
+        filetypes = default_exclude_filetypes,
+    },
 })
