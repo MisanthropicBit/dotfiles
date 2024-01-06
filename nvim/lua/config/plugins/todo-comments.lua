a7685f29597ce4bb7f0641df006419333ced228d
aab <aab@connectedcars.dk>
2024-01-06 15:17:08 +0100

    Jump between todos

--- nvim/lua/config/plugins/todo-comments.lua
+++ nvim/lua/config/plugins/todo-comments.lua
@@ -1,6 +1,20 @@
-require("todo-comments").setup({
+local todo_comments = require("todo-comments")
+
+local map = require("config.map")
+
+todo_comments.setup({
     sign_priority = (vim.g.gitgutter_sign_priority or 10) + 1,
     keywords = {
         IMPORTANT = "WARN",
     },
 })
+
+local function jump_and_center(func)
+    return function()
+        func()
+        vim.cmd.normal("zz")
+    end
+end
+
+map.leader("n", "dn", jump_and_center(todo_comments.jump_next), "Jump to next todo comment")
+map.leader("n", "dp", jump_and_center(todo_comments.jump_prev), "Jump to previous todo comment")
