47c86f99b810a4973b5da7151388de77f5faa79e
aab <aab@connectedcars.dk>
2024-01-30 13:00:27 +0100

    Export autocmds config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,5 @@
+local autocmds = {}
+
 local augroup = vim.api.nvim_create_augroup("config", { clear = true })
 
 if vim.fn.exists("g:last_tab") == 0 then
@@ -6,26 +8,30 @@ end
 
 ---@param event string | string[]
 ---@param options table
-local create_config_autocmd = function(event, options)
+autocmds.create_config_autocmd = function(event, options)
     vim.api.nvim_create_autocmd(event, vim.tbl_extend("force", { group = augroup }, options))
 end
 
-create_config_autocmd("TabNew", {
+autocmds.create_config_autocmd("TabNew", {
+    pattern = "*",
+    callback = function()
+        vim.cmd([[BufferLineSortByTabs]])
+    end,
+})
+
+autocmds.create_config_autocmd("TabLeave", {
     pattern = "*",
     callback = function()
         vim.g.last_tab = vim.fn.tabpagenr()
     end,
 })
 
-create_config_autocmd("TabLeave", {
-    pattern = "*",
-    callback = function()
-        vim.g.last_tab = vim.fn.tabpagenr()
-    end,
-})
+-- autocmds.create_config_autocmd("VimResized", {
+--     pattern = "*",
+--     command = "tabdo wincmd =",
+-- })
 
 -- nvim_create_autocmd("InsertEnter", {
---     group = augroup,
 --     pattern = "*",
 --     callback = function()
 --         vim.opt.listchars:append({ "trail:⌴", "nbsp:¬" })
@@ -39,22 +45,22 @@ create_config_autocmd("TabLeave", {
 --     end,
 -- })
 
-create_config_autocmd({ "BufRead", "BufNewFile" }, {
+autocmds.create_config_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*.fsproj",
     command = "setlocal ft=xml",
 })
 
-create_config_autocmd({ "BufRead", "BufNewFile" }, {
+autocmds.create_config_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = { "*.html", "*.yml", "*.json" },
     command = "setlocal sw=2",
 })
 
-create_config_autocmd({ "BufRead", "BufNewFile" }, {
+autocmds.create_config_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = ".eslintrc",
     command = "setlocal ft=json",
 })
 
-create_config_autocmd("TextYankPost", {
+autocmds.create_config_autocmd("TextYankPost", {
     pattern = "*",
     callback = function()
         vim.highlight.on_yank({
@@ -64,11 +70,11 @@ create_config_autocmd("TextYankPost", {
     end,
 })
 
-create_config_autocmd("TermOpen", {
+autocmds.create_config_autocmd("TermOpen", {
     command = "setlocal signcolumn=no nospell",
 })
 
-create_config_autocmd("TermClose", {
+autocmds.create_config_autocmd("TermClose", {
     callback = function()
         if vim.v.event.status == 0 then
             vim.api.nvim_buf_delete(0, {})
@@ -76,7 +82,7 @@ create_config_autocmd("TermClose", {
     end,
 })
 
-create_config_autocmd("Colorscheme", {
+autocmds.create_config_autocmd("Colorscheme", {
     callback = function()
         -- Better highlights for window separators when statusline is global
         if vim.o.laststatus == 3 then
@@ -87,3 +93,5 @@ create_config_autocmd("Colorscheme", {
         end
     end,
 })
+
+return autocmds
