044075a8a23f73afd3ac0b9bf3f755abd4176292
MisanthropicBit <albo.developer@gmail.com>
2024-02-17 12:05:34 +0100

    Add new line and start insert mode when committing

--- nvim/after/ftplugin/fugitive.lua
+++ nvim/after/ftplugin/fugitive.lua
@@ -4,13 +4,18 @@ local function normal_cmd_with_restore_window(cmd)
     return function()
         local winview = vim.fn.winsaveview()
 
-        vim.cmd((':silent execute "normal %s"'):format(cmd))
+        vim.cmd(('<cmd>silent execute "normal %s"'):format(cmd))
 
         vim.fn.winrestview(winview)
     end
 end
 
-map.n("cvv", "<cmd>vert G commit<cr>", { buffer = true })
+map.n("cvv", function()
+    vim.cmd("vert G commit")
+    vim.cmd("norm! O")
+    vim.cmd.startinsert()
+end, { buffer = true })
+
 map.n("-", normal_cmd_with_restore_window([[\<Plug>fugitive:-zz]]), { buffer = true })
 
 -- I go to unstaged files way more often than untracked files
