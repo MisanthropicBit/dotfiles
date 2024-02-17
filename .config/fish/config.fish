2befbdc1d5f5b345553aa9e80ebcdec8a22be28a
MisanthropicBit <albo.developer@gmail.com>
2024-02-17 12:13:29 +0100

    Add lua paths to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -59,11 +59,14 @@ set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --search-path \$dir"
 set -x FZF_CTRL_T_OPTS '--multi --bind="ctrl-s:select,ctrl-u:deselect,ctrl-f:reload(find . -type f \$dir),ctrl-d:reload(find . -type d \$dir)" --preview="bat -n --color=always {}" --cycle'
 set -x FZF_ALT_C_COMMAND 'fd -td --color=never'
 set -x FZF_DEFAULT_OPTS "--color='pointer:bright-blue,marker:bright-green' --height=50% --pointer='⇨ ' --marker='✓' --bind='ctrl-n:preview-page-down,ctrl-p:preview-page-up' --cycle"
+set -x LUA_PATH '/opt/local/share/lua/5.1/?.lua;/opt/local/share/lua/5.1/?/init.lua;/opt/local/lib/lua/5.1/?.lua;/opt/local/lib/lua/5.1/?/init.lua;./?.lua;./?/init.lua;~/.luarocks/share/lua/5.1/?.lua;~/.luarocks/share/lua/5.1/?/init.lua;/opt/local/share/luarocks/share/lua/5.1/?.lua;/opt/local/share/luarocks/share/lua/5.1/?/init.lua'
+set -x LUA_CPATH '/opt/local/lib/lua/5.1/?.so;/opt/local/lib/lua/5.1/loadall.so;./?.so;~/.luarocks/lib/lua/5.1/?.so;/opt/local/share/luarocks/lib/lua/5.1/?.so'
 
 fish_add_path /opt/local/bin /usr/local/bin /opt/homebrew/bin
 fish_add_path ~/.npm-global/bin "$PYENV_ROOT/bin" ~/projects/c/terminal_blocks
 fish_add_path ~/google-cloud-sdk/bin ~/repos/cloud-sql-proxy/
 fish_add_path -p "$N_PREFIX/bin"
+fish_add_path ~/.luarocks/bin /opt/local/share/luarocks/bin
 
 bind \cb git_fzf_branches
 bind \cg git_fzf_log
