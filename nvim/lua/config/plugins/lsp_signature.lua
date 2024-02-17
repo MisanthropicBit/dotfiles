ee735a3ec42a2a7c9637acff5da592b73ebf3aaa
MisanthropicBit <albo.developer@gmail.com>
2024-02-17 12:10:03 +0100

    Fix lsp_signature icon

--- nvim/lua/config/plugins/lsp_signature.lua
+++ nvim/lua/config/plugins/lsp_signature.lua
@@ -1,7 +1,9 @@
+local lsp_utils = require("config.lsp.utils")
+
 require("lsp_signature").setup({
     close_timeout = 2000,
     always_trigger = false,
-    hint_prefix = " ﴕ ",
+    hint_prefix = lsp_utils.kind_icons.Method,
     select_signature_key = "<c-s>",
     timer_interval = 100,
 })
