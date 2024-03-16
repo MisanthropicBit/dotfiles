b616ce93b77bb9e3570de9bfad99bbda84e1a98c
aab <aab@connectedcars.dk>
2024-03-16 21:29:25 +0100

    Numbered unicode bullets for headlines.nvim

--- nvim/lua/config/plugins/headlines.lua
+++ nvim/lua/config/plugins/headlines.lua
@@ -1,6 +1,7 @@
 require("headlines").setup({
     markdown = {
         -- Breaks with some patched fonts like RobotoMono Nerd font
-        fat_headlines = false
+        fat_headlines = false,
+        bullets = { "󰲠 ", "󰲢 ", "󰲤 ", "󰲦 " },
     },
 })
