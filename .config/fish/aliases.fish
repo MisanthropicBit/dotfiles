9da080705b05cdf1aa75d4d8288fa78cdcdd447a
aab <aab@connectedcars.dk>
2024-04-05 21:00:31 +0200

    Remove blank line in dh command

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -86,6 +86,7 @@ end
 function dh -d "Fuzzy search directory history"
     set -l result (
         dirh |
+        grep -v '^\s*$' |
         fzf --ansi --with-nth=2 --tiebreak=end --preview='tree -C -L 1 -hp --dirsfirst {1}' |
         string split -f2 ') '
     ); and cd "$result"
