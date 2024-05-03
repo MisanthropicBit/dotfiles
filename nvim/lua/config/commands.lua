a481e20e2f20fcd5f81ab05302b11d22c5f941dc
aab <aab@connectedcars.dk>
2024-05-03 18:48:47 +0200

    Better Grep with ui and escaping

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -1,6 +1,16 @@
-vim.api.nvim_create_user_command("Grep", [[execute 'silent grep! <args> | copen']], {
-    nargs = "+",
-})
+local icons = require("config.icons")
+local ui = require("config.ui")
+
+vim.api.nvim_create_user_command("Grep", function()
+    ui.input.open_default({
+        title = "Grep for " .. icons.misc.prompt,
+    }, function(input)
+        local escaped, _ = input:gsub('([|"])', "\\%1")
+        local command = ([[execute 'silent grep! "%s" | copen']]):format(escaped)
+
+        vim.cmd(command)
+    end)
+end, {})
 
 -- vim supports a 'vertical'/'tab' prefix before :terminal, but neovim
 -- currently doesn't
