d82dbd9427816127f7f8ec6064df8284ef278703
aab <aab@connectedcars.dk>
2024-05-10 22:18:04 +0200

    Cleanup incline setup, update config options

--- nvim/lua/config/plugins/incline.lua
+++ nvim/lua/config/plugins/incline.lua
@@ -21,43 +21,50 @@ local function get_diagnostic_label(props)
     return label
 end
 
+local function render(props)
+    local bufname = vim.api.nvim_buf_get_name(props.buf)
+    local filename = vim.fn.fnamemodify(bufname, ":t")
+
+    if filename == "" then
+        filename = icons.files.new
+    end
+
+    local ft_icon, ft_color = devicons.get_icon_color(filename)
+    local modified = vim.bo[props.buf].modified
+
+    if vim.startswith(bufname, "oil://") then
+        filename = "oil"
+        ft_icon = icons.files.oil
+        ft_color = ({ devicons.get_icon_color_by_filetype("txt") })[2]
+    elseif vim.startswith(bufname, "fugitive://") then
+        filename = "fugitive"
+        ft_icon, ft_color = devicons.get_icon_color_by_filetype("git")
+    end
+
+    return {
+        ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+        " ",
+        { filename, gui = modified and "bold,italic" or "bold" },
+        " ",
+        { modified and (icons.test.running .. " ") or "", group = "diffChanged" },
+        get_diagnostic_label(props),
+        group = "NormalFloat",
+    }
+end
+
 require("incline").setup({
+    hide = {
+        cursorline = true,
+    },
     ignore = {
         buftypes = {},
     },
     window = {
         padding = 0,
         margin = { horizontal = 0 },
+        overlap = {
+            borders = true,
+        },
     },
-    render = function(props)
-        local bufname = vim.api.nvim_buf_get_name(props.buf)
-        local filename = vim.fn.fnamemodify(bufname, ":t")
-
-        if filename == "" then
-            filename = icons.files.new
-        end
-
-        local ft_icon, ft_color = devicons.get_icon_color(filename)
-        local modified = vim.bo[props.buf].modified
-
-        if vim.startswith(bufname, "oil://") then
-            filename = "oil"
-            ft_icon = icons.files.oil
-            ft_color = ({ devicons.get_icon_color_by_filetype("txt") })[2]
-        elseif vim.startswith(bufname, "fugitive://") then
-            filename = "git status"
-            ft_icon, ft_color = devicons.get_icon_color_by_filetype("git")
-        end
-
-        return {
-            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-            " ",
-            { filename, gui = modified and "bold,italic" or "bold" },
-            " ",
-            { modified and icons.test.running or "", group = "diffChanged" },
-            " ",
-            get_diagnostic_label(props),
-            group = "NormalFloat",
-        }
-    end,
+    render = render,
 })
