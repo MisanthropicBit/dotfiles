c14ccc543a176f4b438b64e1ff1e0cf826204756
aab <aab@connectedcars.dk>
2024-07-28 11:27:36 +0200

    Not using gitgutter anymore

--- nvim/lua/config/plugins/todo-comments.lua
+++ nvim/lua/config/plugins/todo-comments.lua
@@ -4,7 +4,6 @@ local map = require("config.map")
 local icons = require("config.icons")
 
 todo_comments.setup({
-    sign_priority = (vim.g.gitgutter_sign_priority or 10) + 1,
     keywords = {
         IMPORTANT = "WARN",
         FIX = {
