0bcd7601a00495cc0d7ad7f4763e2c06c6f7548e
aab <aab@connectedcars.dk>
2024-07-28 11:27:02 +0200

    Reconfigure git-messenger

--- nvim/lua/config/plugins/vim-plugins/git-messenger.lua
+++ nvim/lua/config/plugins/vim-plugins/git-messenger.lua
@@ -1,1 +1,13 @@
 vim.g.git_messenger_conceal_word_diff_marker = true
+vim.g.git_messenger_always_into_popup = true
+
+local autocmds = require("config.autocmds")
+local map = require("config.map")
+
+autocmds.create_config_autocmd("FileType", {
+    pattern = { "gitmessengerpopup" },
+    callback = function(event)
+        map.n("<c-h>", "o", { buffer = event.buf, noremap = false })
+        map.n("<c-l>", "O", { buffer = event.buf, noremap = false })
+    end,
+})
