19aa74f13432ecd4b186c3850b2a3f2846454669
aab <aab@connectedcars.dk>
2024-07-28 11:14:09 +0200

    Keymaps for navigating error lists

--- nvim/after/ftplugin/qf.lua
+++ nvim/after/ftplugin/qf.lua
@@ -2,6 +2,8 @@ local map = require("config.map")
 
 map.n("<c-n>", "<cmd>keepjumps cnext <bar> wincmd p<cr>", { buffer = true })
 map.n("<c-p>", "<cmd>keepjumps cprev <bar> wincmd p<cr>", { buffer = true })
+map.n("<c-h>", "<cmd>colder<cr>", { buffer = true })
+map.n("<c-l>", "<cmd>cnewer<cr>", { buffer = true })
 map.n("qs", "<c-w><cr>", { buffer = true })
 map.n("qv", "<c-w><cr><c-w>L<c-w>p<c-w>J<c-w>p", { buffer = true })
 map.n("qt", "<c-w><cr><c-w>T", { buffer = true })
