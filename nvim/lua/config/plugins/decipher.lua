6376dc840f0443708c25d7a5b909e4a8f1907e55
aab <aab@connectedcars.dk>
2024-07-28 11:36:42 +0200

    Decipher visual keymaps

--- nvim/lua/config/plugins/decipher.lua
+++ nvim/lua/config/plugins/decipher.lua
@@ -21,3 +21,11 @@ end, "Decode an encoded text object")
 map.n("gt", function()
     decipher.encode_motion_prompt({ preview = true })
 end, "Encode a decoded text object")
+
+map.v("gr", function()
+    decipher.decode_motion_prompt({ preview = true })
+end, "Decode an encoded visual selection")
+
+map.v("gt", function()
+    decipher.encode_motion_prompt({ preview = true })
+end, "Encode a visual selection")
