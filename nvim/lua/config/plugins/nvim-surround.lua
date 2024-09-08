c4c42f35a879a8aacf6e6b57cb58ff9c2c438edb
aab <aab@connectedcars.dk>
2024-09-08 20:40:33 +0200

    More easily typeable ')' and '{' surrounds

--- nvim/lua/config/plugins/nvim-surround.lua
+++ nvim/lua/config/plugins/nvim-surround.lua
@@ -1,1 +1,10 @@
-require("nvim-surround").setup({})
+local surround = require("nvim-surround")
+
+local defaults = require("nvim-surround.config").default_opts
+
+surround.setup({
+    surrounds = {
+        ["8"] = defaults.surrounds[")"],
+        ["9"] = defaults.surrounds["{"],
+    },
+})
