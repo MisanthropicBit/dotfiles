8d23e74f23b21498fe16e875492526a8829077b3
MisanthropicBit <proximity.dev@proton.me>
2024-11-07 15:21:33 +0100

    More lua snippets

--- .config/nvim/snippets/lua/lua.lua
+++ .config/nvim/snippets/lua/lua.lua
@@ -5,9 +5,16 @@ local function snake_to_kebab_case(args)
     end
 end
 
+local function lower(args)
+    if args then
+        return args[1][1]:lower()
+    end
+end
+
 return {
     -- General
     s("req", fmt('local {} = require("{}")', { i(1), i(2) })),
+    s("rm", fmt('local {} = require("{}.{}")', { i(1), i(2), f(lower, 1) })),
     s("pc", fmt('local ok, {} = pcall({})', { i(1), i(2) })),
     s("pr", fmt('local has_{}, {} = pcall(require, "{}")', { i(1), rep(1), i(2) })),
     s("lf", fmt([[local function {}({})
@@ -35,10 +42,20 @@ return {}]], { i(1, "M"), i(2), rep(1) })),
     s("vp", fmt("vim.print({})", i(1))),
     s("vi", fmt("vim.inspect({})", i(1))),
     s("vpi", fmt("vim.print(vim.inspect({}))", i(1))),
+    s("vip", fmt("vim.print(vim.inspect({}))", i(1))),
     s("has", fmt("local has_{}, {} = pcall(require, \"{}\")", { i(1), rep(1), f(snake_to_kebab_case, 1) })),
-    s("ti", fmt("table.insert({}, {})", { i(1), i(2)  })),
+    s("ti", fmt("table.insert({}{})", { i(1), c(2, { t(""), sn(nil, { t(", {}") } ) }) })),
+    s("tr", fmt("table.remove({}{})", { i(1), c(2, { t(""), t(", {}") }) })),
+    s("rt", t("return")),
+    s("rn", t("return nil")),
 
     -- Busted
+    s("bfe", fmt([[before_each(function()
+    {}
+end)]], { i(1) })),
+    s("afe", fmt([[after_each(function()
+    {}
+end)]], { i(1) })),
     s("des", fmt([[describe("{}", function()
     {}
 end)]], { i(1), i(2) })),
@@ -48,4 +65,8 @@ end)]], { i(1), i(2) })),
     s("ita", fmt([[async.it("{}", function()
     {}
 end)]], { i(1), i(2) })),
+    s("as", fmt([[assert.are.same({}, {})]], { i(1), i(2) })),
+    s("at", fmt([[assert.is_true({})]], { i(1) })),
+    s("af", fmt([[assert.is_false({})]], { i(1) })),
+    s("an", fmt([[assert.is_nil({})]], { i(1) })),
 }
