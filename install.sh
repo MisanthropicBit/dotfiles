90e0ea94eab073732a5bd4d91898ba706e3bcb7f
MisanthropicBit <proximity.dev@proton.me>
2024-11-07 20:15:43 +0100

    Cleanup/improve/update install script

--- install.sh
+++ install.sh
@@ -1,44 +1,73 @@
 #!/usr/bin/env bash
 
-files=".aliases .config .gitconfig .inputrc .latexmkrc .vimrc .vim"
+config_files=(
+    ".gitconfig"
+    ".gitignore_global",
+    ".inputrc"
+    ".latexmkrc"
+)
 
-# Check for pre-exisiting files
-for file in $files; do
-    if -e "~/.$file"; then
-        printf "File '.$file' already exists in your home directory, aborting...\n"
-        exit 1
-    fi
-done
+config_dirs=(
+    "bat"
+    "bpython"
+    "fish"
+    "nvim"
+)
 
-# Symlink files into user home directory
-for file in $files; do
-    printf "Symlinking '.$file'...\n"
-    ln -s ".$file" "~/.$file"
+script_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-    if [$? -ne 0]; then
-        printf "Failed to symlink '.$file'!"
-    fi
-done
+log() {
+    printf "\x1b[$1m[$2]\x1b[0m $3\n"
+}
 
-__symlink_install() {
+ok() {
+    log "32;1" "info" "$1"
+}
+
+info() {
+    log "37;1" "info" "$1"
+}
+
+warn() {
+    log "33;1" "warn" "$1"
+}
+
+error() {
+    log "31;1" "error" "$1"
+}
+
+prompt() {
+    read -p "$1" -n 1 -r
+    printf "$REPLY"
+}
+
+symlink_install() {
     if [ -e "$3/$1" ]; then
-        echo "Error: Cannot symlink $1 because it already exists in $3"
+        warn "$1 already exists at $3"
     else
         ln -s "$2/$1" "$3/$1"
 
-        if [ $? != 0 ]; then
-            echo "Error: Failed to symlink $1 (return code: $?)"
+        if [ $? == 0 ]; then
+            ok "Symlinked $1"
+        else
+            error "Failed to symlink $1 (return code: $?)"
         fi
     fi
 }
 
-script_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+info "Symlinking from '$script_dir' into '$HOME'..."
+reply="$(prompt "Is this the correct directory? ")"
 
-echo "Symlinking files from $script_dir..."
+if [[ ! $reply =~ ^[Yy]$ ]]; then
+    exit 1
+fi
 
-for e in ".aliases" ".gitconfig" ".vimrc" ".vim"; do
-    __symlink_install "$e" "$script_dir" "$HOME"
+printf "\n"
+
+for file in "${config_files[@]}"; do
+    symlink_install "$file" "$script_dir" "$HOME"
 done
 
-# Update .vim/bundle submodules
-git submodule update --init
+for dir in "${config_dirs[@]}"; do
+    symlink_install "$dir" "$script_dir" "$HOME/.config"
+done
