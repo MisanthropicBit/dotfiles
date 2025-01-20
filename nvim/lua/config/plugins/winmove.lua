32b0a2242e4cdcbd8e576c9cba09a862b0a25514
aab <aab@connectedcars.dk>
2025-01-20 19:49:01 +0100

    winmove.nvim setup

--- nvim/lua/config/plugins/winmove.lua
+++ nvim/lua/config/plugins/winmove.lua
@@ -1,1 +1,23 @@
-return { dir = "~/projects/nvim/winmove.nvim" }
+return {
+    "MisanthropicBit/winmove.nvim",
+    config = function()
+        local winmove = require("winmove")
+
+        winmove.configure({
+            modes = {
+                move = {
+                    at_edge = {
+                        horizontal = winmove.AtEdge.MoveToTab,
+                        vertical = winmove.AtEdge.Wrap,
+                    },
+                },
+                swap = {
+                    at_edge = {
+                        horizontal = winmove.AtEdge.MoveToTab,
+                        vertical = winmove.AtEdge.Wrap,
+                    },
+                },
+            },
+        })
+    end
+}
