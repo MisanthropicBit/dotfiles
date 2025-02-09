5a19eab0778e0f0b81d69fde172dece0b1a00841
MisanthropicBit <proximity.dev@proton.me>
2025-02-09 20:23:46 +0100

    Make luasnip depend on friendly-snippets

--- nvim/lua/config/plugins/luasnip.lua
+++ nvim/lua/config/plugins/luasnip.lua
@@ -1,5 +1,6 @@
 return {
     "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
         local luasnip = require("luasnip")
         local loaders = require("luasnip.loaders")
