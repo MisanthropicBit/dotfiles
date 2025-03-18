3d33146a7e2942d6798caf0c068514616a39cae6
MisanthropicBit <proximity.dev@proton.me>
2025-03-18 15:03:57 +0100

    Add overseer task for ninja

--- nvim/lua/config/plugins/overseer.lua
+++ nvim/lua/config/plugins/overseer.lua
@@ -16,5 +16,22 @@ return {
 
         map.n.leader("or", "<cmd>OverseerRun<cr>", "Run a task using overseer")
         map.n.leader("oo", "<cmd>OverseerToggle<cr>", "Toggle the overseer window")
+
+        overseer.register_template({
+            name = "Ninja",
+            builder = function(params)
+                return {
+                    cmd = { "ninja" },
+                }
+            end,
+            desc = "Build a C/C++ project with ninja",
+            tags = { overseer.TAG.BUILD },
+            condition = {
+                filetype = { "c", "cpp" },
+                callback = function()
+                    return vim.fn.executable("ninja") == 1
+                end,
+            }
+        })
     end,
 }
