b38f5aa7fe2631730fa0ab21e462ef740cc81eb4
aab <aab@connectedcars.dk>
2025-05-15 21:34:08 +0200

    New kubectl abbreviations, tofu abbreviations

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -3,13 +3,15 @@ function is_kubectl_command -a token
         "cc"\
         "shell"\
         "taints"\
-        "podevents"
+        "podevents"\
+        "des"
 
     set -f kubectl_alias_values\
         "config current-context"\
         "exec --stdin --tty % -- /bin/bash"\
         "get node % -o jsonpath='{.metadata.labels} {.spec.taints}' | jq"\
-        "events --for=pod/%"
+        "events --for=pod/%"\
+        "describe"
 
     set -l is_kubectl_cmd (string match --regex "^(kc|kubectl)" (commandline --current-buffer))
 
@@ -28,3 +30,32 @@ abbr --add cc --position anywhere --function is_kubectl_command
 abbr --add shell --position anywhere --function is_kubectl_command --set-cursor="%"
 abbr --add taints --position anywhere --function is_kubectl_command --set-cursor="%"
 abbr --add podevents --position anywhere --function is_kubectl_command --set-cursor="%"
+abbr --add des --position anywhere --function is_kubectl_command
+
+function is_tofu_command -a token
+    set -f tofu_alias_keys\
+        "ws"\
+        "list"\
+        "show"
+
+    set -f tofu_alias_values\
+        "workspace"\
+        "workspace list"\
+        "workspace show"
+
+    set -l is_tofu_cmd (string match --regex "^tofu" (commandline --current-buffer))
+
+    if test -n "$is_tofu_cmd" 
+        if set -l index (contains --index -- "$token" $tofu_alias_keys)
+            echo $tofu_alias_values[$index]
+        else
+            echo $token
+        end
+    else
+        echo $token
+    end
+end
+
+abbr --add ws --position anywhere --function is_tofu_command
+abbr --add list --position anywhere --function is_tofu_command
+abbr --add show --position anywhere --function is_tofu_command
