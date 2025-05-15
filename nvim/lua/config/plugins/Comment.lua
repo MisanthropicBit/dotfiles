31772601a71037fc141771571b86d03b47c7f112
aab <aab@connectedcars.dk>
2025-05-15 21:55:53 +0200

    Move comment keymap to plugin

--- nvim/lua/config/plugins/Comment.lua
+++ nvim/lua/config/plugins/Comment.lua
@@ -1,1 +1,11 @@
-return { "numToStr/Comment.nvim", opts = {} }
+return {
+    "numToStr/Comment.nvim",
+    config = function()
+        local map = require("config.map")
+
+        map.n("ycc", "yygccp", {
+            remap = true,
+            desc = "Copy line below and comment original line"
+        })
+    end,
+}
