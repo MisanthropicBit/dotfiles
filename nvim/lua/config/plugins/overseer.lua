6f065cd97e13ee9a84b16dc7260f20f1dab73bdf
aab <aab@connectedcars.dk>
2025-06-11 20:46:58 +0200

    Add overseer task for "npa all"

--- nvim/lua/config/plugins/overseer.lua
+++ nvim/lua/config/plugins/overseer.lua
@@ -18,13 +18,33 @@ return {
         map.n.leader("oo", "<cmd>OverseerToggle<cr>", "Toggle the overseer window")
 
         overseer.register_template({
+            name = "npm run all",
+            ---@diagnostic disable-next-line: unused-local
+            builder = function(params)
+                return {
+                    cmd = { vim.env.SHELL },
+                    args = { "-c", "npa all -c" },
+                }
+            end,
+            desc = "run npm build, test, and lint scripts",
+            tags = { overseer.TAG.BUILD },
+            condition = {
+                filetype = { "javascript", "typescript" },
+                callback = function()
+                    return vim.fs.root(0, { "package.json" }) ~= nil
+                end,
+            }
+        })
+
+        overseer.register_template({
             name = "Ninja",
+            ---@diagnostic disable-next-line: unused-local
             builder = function(params)
                 return {
                     cmd = { "ninja" },
                 }
             end,
-            desc = "Build a C/C++ project with ninja",
+            desc = "build a C/C++ project with ninja",
             tags = { overseer.TAG.BUILD },
             condition = {
                 filetype = { "c", "cpp" },
