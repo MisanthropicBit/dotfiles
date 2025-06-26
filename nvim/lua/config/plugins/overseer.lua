3155deb29ee315955e932125b1441360e65ca37b
aab <aab@connectedcars.dk>
2025-06-26 22:04:24 +0200

    Overseer task template for npm install

--- nvim/lua/config/plugins/overseer.lua
+++ nvim/lua/config/plugins/overseer.lua
@@ -14,8 +14,26 @@ return {
             }
         })
 
+        vim.api.nvim_create_user_command("OverseerRestartLast", function()
+            local tasks = overseer.list_tasks({ recent_first = true })
+
+            if vim.tbl_isempty(tasks) then
+                vim.notify("No tasks found", vim.log.levels.WARN)
+            else
+                overseer.run_action(tasks[1], "restart")
+            end
+        end, {})
+
         map.n.leader("or", "<cmd>OverseerRun<cr>", "Run a task using overseer")
         map.n.leader("oo", "<cmd>OverseerToggle<cr>", "Toggle the overseer window")
+        map.n.leader("ol", "<cmd>OverseerRestartLast<cr>", "Restart the last task if any")
+
+        ---@param options overseer.SearchParams
+        ---@return boolean
+        ---@diagnostic disable-next-line: unused-local
+        local function has_package_json_root(options)
+            return vim.fs.root(0, { "package.json" }) ~= nil
+        end
 
         overseer.register_template({
             name = "npm run all",
@@ -30,10 +48,31 @@ return {
             tags = { overseer.TAG.BUILD },
             condition = {
                 filetype = { "javascript", "typescript" },
-                callback = function()
-                    return vim.fs.root(0, { "package.json" }) ~= nil
-                end,
-            }
+                callback = has_package_json_root,
+            },
+        })
+
+        overseer.register_template({
+            name = "npm install",
+            ---@diagnostic disable-next-line: unused-local
+            builder = function(params)
+                return {
+                    cmd = { vim.env.SHELL },
+                    args = { "-c", "npm i" },
+                }
+            end,
+            desc = "Install packages",
+            tags = { overseer.TAG.BUILD },
+            condition = {
+                filetype = { "javascript", "typescript", "json" },
+                callback = function(options)
+                    if options.filetype == "json" then
+                        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t") == "package.json"
+                    end
+
+                    return has_package_json_root(options)
+                end
+            },
         })
 
         overseer.register_template({
@@ -51,7 +90,7 @@ return {
                 callback = function()
                     return vim.fn.executable("ninja") == 1
                 end,
-            }
+            },
         })
     end,
 }
