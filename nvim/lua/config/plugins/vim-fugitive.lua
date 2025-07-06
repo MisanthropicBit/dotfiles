45359d01e7028ad7c4f5eb4c1e559eeac4fe2b5e
aab <aab@connectedcars.dk>
2025-07-06 12:17:57 +0200

    Clean up vim-fugitive keymaps/commands

--- nvim/lua/config/plugins/vim-fugitive.lua
+++ nvim/lua/config/plugins/vim-fugitive.lua
@@ -1,20 +1,17 @@
 return {
-    "tpope/vim-fugitive", 
+    "tpope/vim-fugitive",
     config = function()
         local map = require("config.map")
 
         -- Useful git mappings based on the spf13-vim distribution
         map.n.leader("gs", "<cmd>G<cr>")
+        map.n.leader("gv", "<cmd>vert G<cr>")
         map.n.leader("gd", "<cmd>Gdiffsplit<cr>")
         map.n.leader("gc", "<cmd>G commit<cr>")
         map.n.leader("gb", "<cmd>G blame<cr>")
         map.n.leader("gp", "<cmd>G push<cr>")
-        map.n.leader("gv", "<cmd>vert G --paginate diff --cached<cr>")
         map.n.leader("gw", "<cmd>Gwrite<cr>")
         map.n.leader("gu", "<cmd>GBrowse!<cr>")
-        map.n.leader("gos", "<cmd>Gsp origin/master:%<cr>")
-        map.n.leader("gov", "<cmd>Gvs origin/master:%<cr>")
-        map.n.leader("got", "<cmd>Gtabedit origin/master:%<cr>")
         map.n.leader("ms", "<cmd>Gsplit origin/master:%<cr>")
         map.n.leader("mv", "<cmd>Gvsplit origin/master:%<cr>")
         map.n.leader("mt", "<cmd>Gtabedit origin/master:%<cr>")
@@ -25,7 +22,5 @@ return {
         map.n("glt", "<cmd>tab Term git log<cr>")
 
         map.c("<c-b>", "<c-r>=printf('origin/%s', FugitiveHead())<cr>", { silent = false })
-
-        vim.api.nvim_create_user_command("Gom", "<cmd><mods> Gsp origin/master:%", {})
     end,
 }
