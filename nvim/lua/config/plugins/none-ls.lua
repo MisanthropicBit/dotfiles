a3cd687751f5500ddbb895314eff1eb80ee3d920
aab <aab@connectedcars.dk>
2025-08-08 09:30:57 +0200

    Remove old null-ls commands

--- nvim/lua/config/plugins/none-ls.lua
+++ nvim/lua/config/plugins/none-ls.lua
@@ -32,37 +32,5 @@ return {
         end
 
         null_ls.setup({ sources = sources })
-
-        local function null_ls_command(command_func)
-            return function()
-                local null_ls_client
-
-                for _, client in ipairs(vim.lsp.get_clients()) do
-                    if client.name == "null-ls" then
-                        null_ls_client = client
-                    end
-                end
-
-                if not null_ls_client then
-                    return
-                end
-
-                command_func(null_ls_client)
-            end
-        end
-
-        local function null_ls_stop(client)
-            if not client.is_stopped() then
-                client.stop()
-            end
-        end
-
-        local function null_ls_restart(client)
-            null_ls_stop(client)
-            vim.lsp.start(client.config)
-        end
-
-        vim.api.nvim_create_user_command("NullLsStop", null_ls_command(null_ls_stop), {})
-        vim.api.nvim_create_user_command("NullLsRestart", null_ls_command(null_ls_restart), {})
     end,
 }
