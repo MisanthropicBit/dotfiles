6fb9afe048dc0eca2a5cd53a87004f6a5e6861cb
aab <aab@connectedcars.dk>
2025-09-04 09:47:10 +0200

    Update sort.nvim config after upgrade

--- nvim/lua/config/plugins/sort.lua
+++ nvim/lua/config/plugins/sort.lua
@@ -1,22 +1,4 @@
 return {
-    "sQVe/sort.nvim", 
-    config = function()
-        local map = require("config.map")
-
-        require("sort").setup()
-
-        local function sort_check_range()
-            local start, _end = vim.fn.getpos("'<")[2], vim.fn.getpos("'>")[2]
-
-            if start ~= _end then
-                vim.notify("Cannot use operator-pending sort over multiple lines")
-                return
-            end
-
-            vim.cmd([[Sort<cr>]])
-        end
-
-        -- TODO: Keymap conflicts
-        map.o('go', sort_check_range, "Sort single-line text-object")
-    end,
+    "sQVe/sort.nvim",
+    opts = {},
 }
