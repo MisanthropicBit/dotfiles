2ba8dad7be1df886b1a86dfa2d29de67274e3f51
aab <aab@connectedcars.dk>
2025-11-12 09:37:50 +0100

    wip insert mapping

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -5,6 +5,7 @@ vim.opt_local.colorcolumn = "50"
 
 local options = { buffer = true, expr = true }
 
+map.i("wip", function() return "WIP [skip ci]" end, options)
 map.i("skci", function() return "[skip ci]" end, options)
 map.i("cisk", function() return "[ci skip]" end, options)
 map.i("icom", function() return "Initial commit" end, options)
