440860e81917a2d4cfa139ef67855b9298f6cb2f
aab <aab@connectedcars.dk>
2026-03-01 20:19:25 +0100

    Add insert mode mapping for negated lua comparison

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,3 +1,6 @@
 vim.o.formatprg = "stylua -"
 vim.o.include = [==[^.*require\s*(\{0,1\}["']\zs[^"']\+\ze["']]==]
+
+vim.keymap.set("i", "!=", "~=")
+
 -- vim.o.includeexpr = "v:lua.require('dotfiles.lua').includeexpr(v:fname)"
