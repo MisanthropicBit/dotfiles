d126c38716e339d1553006e6757bf97ef9767035
aab <aab@connectedcars.dk>
2026-05-02 21:09:15 +0200

    Autocmd updates

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,22 +4,11 @@ local map = require("config.map")
 
 local augroup = vim.api.nvim_create_augroup("config", { clear = true })
 
-if vim.fn.exists("g:last_tab") == 0 then
-    vim.g.last_tab = vim.fn.tabpagenr()
-end
-
 ---@param event string | string[]
 ---@param options vim.api.keyset.create_autocmd
 autocmds.create_config_autocmd = function(event, options)
     vim.api.nvim_create_autocmd(event, vim.tbl_extend("force", options, { group = augroup }))
 end
-
-autocmds.create_config_autocmd("TabLeave", {
-    pattern = "*",
-    callback = function()
-        vim.g.last_tab = vim.fn.tabpagenr()
-    end,
-})
 
 -- nvim_create_autocmd("InsertEnter", {
 --     pattern = "*",
@@ -34,11 +23,6 @@ autocmds.create_config_autocmd("TabLeave", {
 --         vim.opt.listchars:remove({ "trail:⌴", "nbsp:¬" })
 --     end,
 -- })
-
-autocmds.create_config_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.html", "*.yml", "*.json" },
-    command = "setlocal sw=2",
-})
 
 autocmds.create_config_autocmd("TextYankPost", {
     pattern = "*",
@@ -76,17 +60,13 @@ autocmds.create_config_autocmd("TermClose", {
 })
 
 autocmds.create_config_autocmd("Colorscheme", {
-    callback = function(event)
+    callback = function()
         -- Better highlights for window separators when statusline is global
         if vim.o.laststatus == 3 then
             local hl_options = { link = "Keyword", default = false }
 
             vim.api.nvim_set_hl(0, "VertSplit", hl_options)
             vim.api.nvim_set_hl(0, "WinSeparator", hl_options)
-        end
-
-        if event.match == "calvera" then
-            vim.cmd([[silent hi! link IblIndent Comment]])
         end
     end,
 })
@@ -119,7 +99,7 @@ autocmds.create_config_autocmd("FileType", {
 
         vim.opt_local.conceallevel = 2
     end,
-    desc = "Disable diagnostics in jest snapshot files"
+    desc = "Disable diagnostics and conceal escaped backticks in jest snapshot files"
 })
 
 autocmds.create_config_autocmd("BufReadPost", {
