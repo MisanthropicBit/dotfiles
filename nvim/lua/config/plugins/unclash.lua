88b5eb275e96c2d21adcf87ed521417e402127c0
MisanthropicBit <proximity.dev@proton.me>
2026-06-18 09:52:09 +0200

    Add unclash keymaps

--- nvim/lua/config/plugins/unclash.lua
+++ nvim/lua/config/plugins/unclash.lua
@@ -1,13 +1,26 @@
 return {
     src = "https://github.com/madmaxieee/unclash.nvim",
     data = {
-        config = {
-            action_buttons = {
-                enabled = false,
-            },
-            annotations = {
-                enabled = true,
-            },
-        },
+        config = function(unclash)
+            local map = require("config.map")
+
+            unclash.setup({
+                config = {
+                    action_buttons = {
+                        enabled = false,
+                    },
+                    annotations = {
+                        enabled = true,
+                    },
+                },
+            })
+
+            map.n.leader("uc", unclash.accept_current)
+            map.n.leader("ui", unclash.accept_incoming)
+            map.n.leader("ub", unclash.accept_both)
+            map.n.leader("un", unclash.next_conflict)
+            map.n.leader("up", unclash.prev_conflict)
+            map.n.leader("uo", unclash.open_merge_editor)
+        end,
     },
 }
