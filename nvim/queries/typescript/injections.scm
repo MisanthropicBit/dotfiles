d004a2fb49feeeca01390a6998e29e75030da717
MisanthropicBit <proximity.dev@proton.me>
2026-06-18 09:56:31 +0200

    Add more sql injection highlighting

--- nvim/queries/typescript/injections.scm
+++ nvim/queries/typescript/injections.scm
@@ -13,3 +13,73 @@
     (string)
   )
 )
+
+; TODO: Combine the queries below
+
+; Match async call expressions in a variable declarator (template string)
+(call_expression
+  function: (await_expression
+    (member_expression
+      object: (identifier)
+      property: (property_identifier) @method (#eq? @method "query")
+    ))
+    arguments: (arguments
+        ((template_string) @injection.content
+         (#match? @injection.content "^.(select|insert|update|delete|truncate|SELECT|INSERT|UPDATE|DELETE|TRUNCATE)")
+         (#set! injection.language "sql")
+         (#set! injection.include-children)
+         (#offset! @injection.content 0 1 0 -1)
+        )
+    )
+)
+
+; Match async call expressions in a variable declarator (string)
+(call_expression
+  function: (await_expression
+    (member_expression
+      object: (identifier)
+      property: (property_identifier) @method (#eq? @method "query")
+    ))
+    arguments: (arguments
+        ((string) @injection.content
+         (#match? @injection.content "^.(select|insert|update|delete|truncate|SELECT|INSERT|UPDATE|DELETE|TRUNCATE)")
+         (#set! injection.language "sql")
+         (#set! injection.include-children)
+         (#offset! @injection.content 0 1 0 -1)
+        )
+    )
+)
+
+; Match bare async call expressions (template string)
+(call_expression
+  function: (
+    (member_expression
+      object: (identifier)
+      property: (property_identifier) @method (#eq? @method "query")
+    ))
+    arguments: (arguments
+      ((template_string) @injection.content
+        (#match? @injection.content "^.(select|insert|update|delete|truncate|SELECT|INSERT|UPDATE|DELETE|TRUNCATE)")
+        (#set! injection.language "sql")
+        (#set! injection.include-children)
+        (#offset! @injection.content 0 1 0 -1)
+      )
+    )
+)
+
+; Match bare async call expressions (string)
+(call_expression
+  function: (
+    (member_expression
+      object: (identifier)
+      property: (property_identifier) @method (#eq? @method "query")
+    ))
+    arguments: (arguments
+      ((string) @injection.content
+        (#match? @injection.content "^.(select|insert|update|delete|truncate|SELECT|INSERT|UPDATE|DELETE|TRUNCATE)")
+        (#set! injection.language "sql")
+        (#set! injection.include-children)
+        (#offset! @injection.content 0 1 0 -1)
+      )
+    )
+)
