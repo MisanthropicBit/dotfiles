3d26066d3916754b62b4ca069fa258135a884595
MisanthropicBit <proximity.dev@proton.me>
2026-08-03 20:30:09 +0200

    Add c++ commands for config.run

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,3 +1,8 @@
-require("config.c-like-after")
+-- require("config.c-like-after")
+
+vim.b.run = {
+    "ninja",
+    "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 . -G Ninja",
+}
 
 vim.o.makeprg = "ninja"
